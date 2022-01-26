e5d98f3405c98881f0455c48a29523eea1d4ee91
MaxAcheson <maxacheson@gmail.com>
2022-01-25 23:22:39 -0800

    Added coding that outlines shape and location of left hand paddle.

--- PongGame.py
+++ PongGame.py
@@ -8,3 +8,12 @@ game_window.title("Pong by Max")
 game_window.bgcolor("black")
 game_window.setup(width=800, height=600)
 game_window.tracer(0)
+
+# Coding for Left Paddle
+left_paddle = turtle.Turtle()
+left_paddle.speed(0)
+left_paddle.shape("square")
+left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+left_paddle.color("white")
+left_paddle.penup()
+left_paddle.goto(-370,0)
