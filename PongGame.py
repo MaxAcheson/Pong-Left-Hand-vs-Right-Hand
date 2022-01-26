e38eeb8071e2f31431650fd4d69b70338353efc2
MaxAcheson <maxacheson@gmail.com>
2022-01-25 23:21:35 -0800

    Created game program file and added in window parameters, title, and color.

---
+++ PongGame.py
@@ -0,0 +1,10 @@
+# Pong (Left Hand vs Right Hand)
+# By Max Acheson
+
+import turtle
+
+game_window = turtle.Screen()
+game_window.title("Pong by Max")
+game_window.bgcolor("black")
+game_window.setup(width=800, height=600)
+game_window.tracer(0)
